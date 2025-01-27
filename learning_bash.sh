48e314734a76689738b572c4e1b55bb7876db911
Zbahre <tsionredae.bahre@wmich.edu>
2025-01-27 16:01:31 -0500

    Add files via upload

---
+++ learning_bash.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+greetings="Hello"
+name="Zion"
+
+echo "$greetings $name!"
+echo "This script is helping you learn bash."
+
+cd ~/Desktop
+mkdir -p Bioinformatics/bash_scripts
+
+mv ~/Desktop/bash_template.txt ~/Desktop/Bioinformatics/bash_scripts/
+
+mv ~/Desktop/Bioinformatics/bash_scripts/bash_template.txt ~/Desktop/Bioinformatics/bash_scripts/bash_template.sh
+
+
